b31d25f705a6ac0f3fdf055c42b1a758c7f093e9
Brady <bradylill@gmail.com>
2013-09-28 12:48:59 +0800

    Embedding dummy response into /world route.

--- src/clj/immutables/server.clj
+++ src/clj/immutables/server.clj
@@ -6,8 +6,15 @@
             [compojure.route          :as route])
   (:gen-class))
 
+(defn render-world []
+  (str {:object [{:location [15 12]
+                  :name     "dude1"}
+                 {:location [10 10]
+                  :name     "dude2"}]}))
+
 (defroutes app
   (GET "/" [] (response/file-response "game.html" {:root "resources/public"}))
+  (GET "/world" [] (response/response (render-world)))
   (route/not-found (response/file-response "help.html" {:root "resources/public"})))
 
 (defn -main [& args]
