918d93d2529296498e697982332c00a7ceece920
Brady <bradylill@gmail.com>
2013-09-28 12:30:56 +0800

    Hooking up compojure.

--- src/clj/immutables/server.clj
+++ src/clj/immutables/server.clj
@@ -1,35 +1,14 @@
 (ns immutables.server
-  (:require [ring.adapter.jetty :as jetty]
+  (:require [ring.adapter.jetty       :as jetty]
             [ring.middleware.resource :as resources]
-            [ring.util.response :as response])
+            [ring.util.response       :as response]
+            [compojure.core           :refer :all]
+            [compojure.route          :as route])
   (:gen-class))
 
-(defn render-app []
-  (response/redirect "/game.html")
-  #_{:status 200
-   :headers {"Content-Type" "text/html"}
-   :body
-   (str "<!DOCTYPE html>"
-        "<html>"
-        "<head>"
-        "<link rel=\"stylesheet\" href=\"css/page.css\" />"
-        "</head>"
-        "<body>"
-        "<div>"
-        "<p id=\"clickable\">Click me!</p>"
-        "</div>"
-        "<script src=\"js/cljs.js\"></script>"
-        "</body>"
-        "</html>")})
-
-(defn handler [request]
-  (if (= "/" (:uri request))
-      (response/redirect "/help.html")
-      (render-app)))
-
-(def app 
-  (-> handler
-    (resources/wrap-resource "public")))
+(defroutes app
+  (GET "/" [] (response/file-response "game.html" {:root "resources/public"}))
+  (route/not-found (response/file-response "help.html" {:root "resources/public"})))
 
 (defn -main [& args]
   (jetty/run-jetty app {:port 3000}))
