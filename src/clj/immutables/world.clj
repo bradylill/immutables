9c0823e448d62b1bbbaafcd75dc1248febf65867
Jean <jdarc@thoughtworks.com>
2013-09-28 16:59:16 +1000

    added a few more dudes

--- src/clj/immutables/world.clj
+++ src/clj/immutables/world.clj
@@ -2,10 +2,19 @@
 
 (def world (atom {:objects []}))
 
-(def starting-world {:objects [{:location [15 12]
-                                :name     "dude1"}
-                               {:location [10 10]
-                                :name     "dude2"}]})
+(def starting-world {:objects [{:location [318.047 409.0] :name "brady"}
+                               {:location [251.368 535.937] :name "korny"}
+                               {:location [251.377 124.457] :name "jean"}
+                               {:location [582.714 334.363] :name "sarah"}
+                               {:location [42.837 89.189] :name "fred"}
+                               {:location [584.84 156.605] :name "bob"}
+                               {:location [264.22 561.742] :name "leo"}
+                               {:location [618.664 178.174] :name "claire"}
+                               {:location [510.834 30.911] :name "cassie"}
+                               {:location [247.985 46.483] :name "mia"}
+                               {:location [142.929 38.12] :name "erlangga"}
+                               {:location [553.962 330.598] :name "steve"}
+                               {:location [335.016 270.246] :name "andy"}]})
 
 (defn render []
   (str @world))
