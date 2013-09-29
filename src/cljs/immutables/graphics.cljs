00f213c64c0eada64c2ef153631aaf1e49ce4fe4
Jean <jdarc@thoughtworks.com>
2013-09-29 18:07:52 +1000

    cleaning up graphics code

--- src/cljs/immutables/graphics.cljs
+++ src/cljs/immutables/graphics.cljs
@@ -2,42 +2,59 @@
   (:require [monet.canvas :refer [get-context] :as canvas]
             [jayq.util :refer [log]]))
 
-(def scale 1.5)
-(def object-vector-shape [[0.8 5.0] [5.0 (- 4.0)] [2.0 (- 5.0)] [1.5 (- 3.0)]
-                          [(- 1.5) (- 3.0)] [(- 2.0) (- 5.0)] [(- 5.0) (- 4.0)] [(- 0.8) 5.0]])
+(def ^:const scale 2.0)
 
-(defn draw-closed-polygon [ctx points]
+(def ^:const target-radius 3.0)
+
+(def ^:const bot-margin 11.0)
+
+(def ^:const energy-colors ["#FF0000" "#FFFF00" "#00FF00"])
+
+(def ^:const bot-shape [
+                         [0.5 5.0]
+                         [5.0 -4.0]
+                         [2.0 -5.0]
+                         [1.5 -3.0]
+                         [-1.5 -3.0]
+                         [-2.0 -5.0]
+                         [-5.0 -4.0]
+                         [-0.5 5.0]
+                        ])
+
+(defn get-bot-color [name]
+  (str "#" (.toUpperCase (.toString (bit-and (bit-or (js/parseInt (str (reverse name)) 36) (bit-not (hash name))) 0xFFFFFF) 16))))
+
+(defn draw-bot-target [ctx x y]
+  (canvas/circle ctx {:x x :y y :r target-radius}))
+
+(defn draw-bot-name [ctx name]
+  (canvas/text ctx {:text name :x (* (.-width (.measureText ctx name)) -0.5) :y (* bot-margin scale)}))
+
+(defn draw-bot-energy [ctx energy]
+  (let [energy-level (min 1.0 (max 0.0 (/ energy 100.0)))]
+    (canvas/fill-style ctx (get energy-colors (int (* energy-level 2.0))))
+    (canvas/fill-rect ctx {:x (- (* bot-margin scale 0.5)) :y (* bot-margin scale -1.0) :w (* energy-level bot-margin scale) :h scale})))
+
+(defn draw-bot-shape [ctx]
   (canvas/begin-path ctx)
-  (doall (map #(canvas/line-to ctx (* scale (first %)) (* scale (second %))) points))
+  (doall (map #(canvas/line-to ctx (* scale (first %)) (* scale (second %))) bot-shape))
   (canvas/close-path ctx)
-  (canvas/stroke ctx)
-  )
+  (canvas/stroke ctx))
 
-(defn draw-energy [ctx energy]
-  (let [energy-normalized (min 1.0 (max 0 (/ energy 100.0)))]
-    (canvas/fill-style ctx (get ["#FF0000" "#FFFF00" "#00FF00"] (int (* energy-normalized 2))))
-    (canvas/fill-rect ctx {:x (- (* 7 scale)) :y (- (* 11 scale)) :w (* energy-normalized (* 14 scale)) :h (* 1 scale)})
-    ))
-
-(defn name-to-color [name]
-  (.toUpperCase (.toString (bit-and (bit-or (js/parseInt (str (reverse name)) 36) (bit-not (hash name))) 0xFFFFFF) 16))
-  )
-
-(defn draw-object [ctx delta {name :name [tx ty] :target [x y] :location [vx vy] :velocity energy :energy}]
-  (canvas/save ctx)
-  (canvas/fill-style ctx (str "#" (name-to-color name)))
-  (canvas/circle ctx {:x tx :y ty :r 2})
-  (canvas/translate ctx (+ x (* vx delta)) (+ y (* vy delta)))
-  (canvas/text ctx {:text name :x (* (.-width (.measureText ctx name)) (- 0.5)) :y (* 14 scale)})
-  (draw-energy ctx energy)
-  (canvas/stroke-style ctx (str "#" (name-to-color name)))
-  (canvas/rotate ctx (Math/atan2 (- vx) vy))
-  (draw-closed-polygon ctx object-vector-shape)
-  (canvas/restore ctx)
-  )
+(defn draw-bot [ctx delta {name :name [tx ty] :target [x y] :location [vx vy] :velocity energy :energy}]
+  (let [bot-color (get-bot-color name)]
+    (canvas/fill-style ctx bot-color)
+    (draw-bot-target ctx tx ty)
+    (canvas/translate ctx (+ x (* vx delta)) (+ y (* vy delta)))
+    (draw-bot-name ctx name)
+    (draw-bot-energy ctx energy)
+    (canvas/stroke-style ctx bot-color)
+    (canvas/rotate ctx (Math/atan2 (- vx) vy))
+    (draw-bot-shape ctx)
+    (. ctx (setTransform 1 0 0 1 0 0))))
 
 (defn draw-map [ctx world delta]
   (canvas/clear-rect ctx {:x 0 :y 0 :w (.-width (.-canvas ctx)) :h (.-height (.-canvas ctx))})
-  (canvas/save ctx)
-  (doall (map (fn [object] (draw-object ctx delta object)) (:bots world)))
-  (canvas/restore ctx))
+  (canvas/font-style ctx (str (int (* scale 5.5)) "pt sans-serif"))
+  (canvas/text-baseline ctx "middle")
+  (doall (map (fn [bot] (draw-bot ctx delta bot)) (:bots world))))
